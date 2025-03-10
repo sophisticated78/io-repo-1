de594dd3a341032925f1693896acc89edd2bdcf7
Jan Markiewicz <84960@student.ue.poznan.pl>
2025-03-10 10:41:36 +0100

    Poprawka.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def zabij_potwora(eliksir: int, olej: int, miecz: int, sila_potwora: int) -> str:
     #Spradzam jaką siłę ma Geralt
     sila_geralta = eliksir + olej + miecz
-    # Sprawdzam czy Geralt ma większą siłę niż 
+    # Sprawdzam czy Geralt ma większą siłę niż potwór
     if sila_geralta > sila_potwora:
         return "Geralt zabił potwora"
     else:
