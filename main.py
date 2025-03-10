f16dee722b6da42654fae4c02e21ef8c8105805f
Jan Markiewicz <84960@student.ue.poznan.pl>
2025-03-10 10:40:08 +0100

    Zadanie wykonane (chyba xd)

---
+++ main.py
@@ -0,0 +1,10 @@
+def zabij_potwora(eliksir: int, olej: int, miecz: int, sila_potwora: int) -> str:
+    #Spradzam jaką siłę ma Geralt
+    sila_geralta = eliksir + olej + miecz
+    # Sprawdzam czy Geralt ma większą siłę niż 
+    if sila_geralta > sila_potwora:
+        return "Geralt zabił potwora"
+    else:
+        return "Geraltowi nie udało się zabić potwora"
+
+print(zabij_potwora(2, 0, 0, 3)) 
